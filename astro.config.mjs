bdddb0346bb9e475aabc52ad69687eff390ae6ae
Luís Arteiro <lucho14garfield@gmail.com>
2025-06-04 02:20:25 +0100

    feat: Adding favicon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import favicons from "astro-favicons";
 
 export default defineConfig({
   vite: {
     assetsInclude: ["**/*.glsl"],
     plugins: [tailwindcss()],
   },
+
+  integrations: [favicons()],
 });
