3dd1408c032aa359c6bd73ec18e94561186a8483
Luís Arteiro <lucho14garfield@gmail.com>
2025-06-04 15:51:48 +0100

    dep: Adding rule for Eslint to remove unused imports.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import unusedImports from "eslint-plugin-unused-imports";
+
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+      "unused-imports/no-unused-imports": "error",
     },
   },
 ];
