1c5f54eb31aff4f0c291b2abe46d2138ce4680ad
Luís Arteiro <lucho14garfield@gmail.com>
2025-06-04 03:34:57 +0100

    feat: Adding webmanifest during build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,26 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import favicons from "astro-favicons";
 
+import webmanifest from "astro-webmanifest";
+
 export default defineConfig({
   vite: {
     assetsInclude: ["**/*.glsl"],
     plugins: [tailwindcss()],
   },
-  integrations: [favicons()],
-    site: 'https://www.djbrianrodrigues.com/',
+  site: "https://www.djbrianrodrigues.com/",
+  integrations: [
+    favicons(),
+    webmanifest({
+      name: "Brian Rodrigues - Portuguese Wedding DJ in NJ",
+      icon: "public/favicon.svg",
+      short_name: "Brian DJ",
+      description:
+        "Professional bilingual DJ with 20+ years experience specializing in weddings, corporate events, and celebrations in New Jersey.",
+      start_url: "/",
+      theme_color: "#263b91",
+      background_color: "#263b91",
+      display: "standalone",
+    }),
+  ],
 });
